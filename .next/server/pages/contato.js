cbfa36ef98c1e261aa9f216034f5757d6ee77392
Luciano Denilson Leal <you@example.com>
2022-07-05 21:53:18 -0300

    first commit

---
+++ .next/server/pages/contato.js
@@ -0,0 +1,380 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 716;
+exports.ids = [716];
+exports.modules = {
+
+/***/ 460:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "getServerSideProps": () => (/* binding */ getServerSideProps),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(701);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(689);
+/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_script__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(440);
+/* harmony import */ var yup__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(yup__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _components_Menu__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(205);
+/* harmony import */ var _components_Footer__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(651);
+
+
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
+
+
+
+
+
+function Contato({
+  data
+}) {
+  const {
+    0: content,
+    1: setContent
+  } = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)({
+    nome: '',
+    whatsapp: '',
+    email: '',
+    endereco: '',
+    equipamento: '',
+    marca: '',
+    modelo: '',
+    defeito: ''
+  });
+  const {
+    0: response,
+    1: setResponse
+  } = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)({
+    formSave: false,
+    type: '',
+    mensagem: ''
+  });
+
+  const onChangeInput = e => setContent(_objectSpread(_objectSpread({}, content), {}, {
+    [e.target.name]: e.target.value
+  }));
+
+  const sendContact = async e => {
+    e.preventDefault();
+    if (!(await validate())) return;
+    setResponse({
+      formSave: true
+    });
+
+    try {
+      const res = await fetch('http://ch-coder.ddns.net:8090/site-msg-contact/add-msg-contact', {
+        method: 'POST',
+        body: JSON.stringify(content),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const responseEnv = await res.json();
+
+      if (responseEnv.erro) {
+        setResponse({
+          formSave: false,
+          type: 'error',
+          mensagem: responseEnv.mensagem
+        });
+      } else {
+        setResponse({
+          formSave: false,
+          type: 'success',
+          mensagem: responseEnv.mensagem
+        });
+        setContent({
+          nome: '',
+          whatsapp: '',
+          email: '',
+          endereco: '',
+          equipamento: '',
+          marca: '',
+          modelo: '',
+          defeito: ''
+        });
+      }
+    } catch (err) {
+      setResponse({
+        formSave: false,
+        type: 'error',
+        mensagem: "Erro: Mensagem de contato não enviando com sucesso, tente novamente!"
+      });
+    }
+  };
+
+  async function validate() {
+    let schema = yup__WEBPACK_IMPORTED_MODULE_4__.object().shape({
+      nome: yup__WEBPACK_IMPORTED_MODULE_4__.string("Erro: Necessário preencher o campo nome!").required("Erro: Necessário preencher o campo nome!"),
+      whatsapp: yup__WEBPACK_IMPORTED_MODULE_4__.string("Erro: Necessário preencher o campo whatsapp!").required("Erro: Necessário preencher o campo whatsapp!"),
+      email: yup__WEBPACK_IMPORTED_MODULE_4__.string("Erro: Necessário preencher o campo email!").required("Erro: Necessário preencher o campo email!"),
+      endereco: yup__WEBPACK_IMPORTED_MODULE_4__.string("Erro: Necessário preencher o campo endereco!").required("Erro: Necessário preencher o campo endereco!"),
+      equipamento: yup__WEBPACK_IMPORTED_MODULE_4__.string("Erro: Necessário preencher o campo equipamento!").required("Erro: Necessário preencher o campo equipamento!")
+    });
+
+    try {
+      await schema.validate(content);
+      return true;
+    } catch (err) {
+      setResponse({
+        type: 'error',
+        mensagem: err.errors
+      });
+      return false;
+    }
+  }
+
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_2___default()), {
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        charSet: "utf-8"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        name: "robots",
+        content: "index, follow"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        name: "description",
+        content: data.dataSeo.description
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        name: "author",
+        content: "Luciano D. Leal - Clinihard Sistemas"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+        name: "viewport",
+        content: "initial-scale=1.0, width=device-width"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("title", {
+        children: data.dataSeo.title
+      })]
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Menu__WEBPACK_IMPORTED_MODULE_5__/* .default */ .Z, {}), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("section", {
+      className: "contact",
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "max-width",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+          className: "title",
+          children: data.dataContact.title_contact
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "contact-content",
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: "column left",
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+              children: data.dataContact.desc_contact
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+              className: "icons",
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "row",
+                children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                  className: data.dataContact.icon_company
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                  className: "info",
+                  children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "head",
+                    children: data.dataContact.title_company
+                  }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "sub-title",
+                    children: data.dataContact.desc_company
+                  })]
+                })]
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "row",
+                children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                  className: data.dataContact.icon_address
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                  className: "info",
+                  children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "head",
+                    children: data.dataContact.title_address
+                  }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "sub-title",
+                    children: data.dataContact.desc_address
+                  })]
+                })]
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "row",
+                children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("i", {
+                  className: data.dataContact.icon_email
+                }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                  className: "info",
+                  children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "head",
+                    children: data.dataContact.title_email
+                  }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                    className: "sub-title",
+                    children: data.dataContact.desc_email
+                  })]
+                })]
+              })]
+            })]
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: "column right",
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+              className: "text",
+              children: data.dataContact.title_form
+            }), response.type === 'error' ? /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+              className: "alert-danger",
+              children: response.mensagem
+            }) : "", response.type === 'success' ? /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+              className: "alert-success",
+              children: response.mensagem
+            }) : "", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", {
+              onSubmit: sendContact,
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "fields",
+                children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                  className: "field nome",
+                  children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                    type: "text",
+                    name: "nome",
+                    placeholder: "Digite o nome",
+                    onChange: onChangeInput,
+                    value: content.nome
+                  })
+                }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                  className: "field whatsapp",
+                  children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                    type: "text",
+                    name: "whatsapp",
+                    placeholder: "Digite o whatsapp",
+                    onChange: onChangeInput,
+                    value: content.whatsapp
+                  })
+                }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                  className: "field email",
+                  children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                    type: "e-mail",
+                    name: "email",
+                    placeholder: "Digite o email",
+                    onChange: onChangeInput,
+                    value: content.email
+                  })
+                })]
+              }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "field endereco",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                  type: "text",
+                  name: "endereco",
+                  placeholder: "Digite o endereco",
+                  onChange: onChangeInput,
+                  value: content.endereco
+                })
+              }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "field equipamento",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                  type: "text",
+                  name: "equipamento",
+                  placeholder: "Digite o equipamento",
+                  onChange: onChangeInput,
+                  value: content.equipamento
+                })
+              }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "field defeito",
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+                  type: "text",
+                  name: "defeito",
+                  placeholder: "Digite o defeito",
+                  onChange: onChangeInput,
+                  value: content.defeito
+                })
+              }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "button-area",
+                children: response.formSave ? /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+                  type: "submit",
+                  disabled: true,
+                  children: "Enviando..."
+                }) : /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+                  type: "submit",
+                  children: "Enviar"
+                })
+              })]
+            })]
+          })]
+        })]
+      })
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Footer__WEBPACK_IMPORTED_MODULE_6__/* .default */ .Z, {
+      data: data.dataFooter
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_script__WEBPACK_IMPORTED_MODULE_3___default()), {
+      src: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+      strategy: "beforeInteractive"
+    }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_script__WEBPACK_IMPORTED_MODULE_3___default()), {
+      src: "custom.js",
+      strategy: "afterInteractive"
+    })]
+  });
+}
+
+async function getServerSideProps() {
+  const response = await fetch(`http://ch-coder.ddns.net:8090/site-cont-contact/view-content-contact`);
+  const data = await response.json(); //console.log(data);
+
+  return {
+    props: {
+      data
+    }
+  };
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Contato);
+
+/***/ }),
+
+/***/ 701:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("next/script");
+
+/***/ }),
+
+/***/ 297:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 282:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 440:
+/***/ ((module) => {
+
+module.exports = require("yup");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [864], () => (__webpack_exec__(460)));
+module.exports = __webpack_exports__;
+
+})();
